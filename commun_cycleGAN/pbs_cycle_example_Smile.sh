ec4fe9248df82c28d6f4989c1d073e064e809a82
PaulinBrissonneau <paulin.25@live.fr>
2020-06-25 17:37:05 +0200

    config ok pour Smile

---
+++ commun_cycleGAN/pbs_cycle_example_Smile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -S /bin/bash
+#PBS -N cycleGAN_example
+#PBS -j oe
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=12:ngpus=1:mem=26gb
+#PBS -q gpuq
+#PBS -P cycleGAN
+#PBS -M paulin.brissonneau@student-cs.fr
+
+# Go to the directory where the job has been submitted 
+cd $PBS_O_WORKDIR
+
+# Module load 
+module load anaconda3/5.3.1
+
+# Activate anaconda environment code
+source activate tf-gpu
+
+# Train the network
+python main.py config_example_gpu_Smile.json
